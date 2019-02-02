d5c525d924c02e3ef66eb77bc9bcd968442ce436
Admin <admin@Intern6-MacBook-Pro.local>
2019-02-02 23:38:03 +0530

    Day3_assignment

--- Day3_assingment/adding_dictonary.py
+++ Day3_assingment/adding_dictonary.py
@@ -1,5 +1,13 @@
 from collections import Counter
-element1 = {'a': 100, 'b': 200, 'c':300}
-element2 = {'a': 300, 'b': 200, 'd':400}
-sum_of_elements = Counter(element1) + Counter(element2)
+ element1 = {'a': 100, 'b': 200, 'c':300}
+ element2 = {'a': 300, 'b': 200, 'd':400}
+ sum_of_elements = Counter(element1) + Counter(element2)
 print(sum_of_elements)
+
+
+
+
+
+
+
+
