7b25091fb631596d4cf34ca4aed419a140a9fb0f
Admin <admin@Intern6-MacBook-Pro.local>
2019-02-02 23:45:59 +0530

    Day2_assignment

--- day2_exercise/repeated_numbers.py
+++ day2_exercise/repeated_numbers.py
@@ -1,17 +1,21 @@
 n = int(input("enter the number of element to be present"))
 my_array = []
+repe_numbers = []
+
+for i in range(1, n + 1):
+    elements = int(input("enter the elements to be present"))
+    my_array.append(int(elements))
+return my_array
+print("list of elements:", my_array)
 
 
-def repeated_number():
-    for i in range(1, n + 1):
-        elements = int(input("enter the elements to be present"))
-        my_array.append(int(elements))
-        print("list of elements:", my_array)
-    my_array.sort()
-    print("sorted elements:", my_array)
 
-    for i in range(len(my_array) - 1):
-       if my_array[i] == my_array[i + 1]:
-         print(my_array[i])
-         print(my_array.count(my_array[i]))
-print(repeated_number())
+def repeated_number(my_array):
+    repe_numbers = {}
+    numbers = {}
+    for i in range(1, set(my_array)):
+        if repe_numbers.count(i) > 0:
+            numbers[i] = repe_number.count(i)
+    for i in range(1, numbers):
+        print('{}{}'.format(i, numbers[i])
+repeated_number(my_array)
