8e86736a22c38a375de1f013e6492f0b0dc64a32
Admin <admin@Intern6-MacBook-Pro.local>
2019-02-02 23:41:40 +0530

    Day3_assignment

--- Day3_assingment/persons_result.py
+++ Day3_assingment/persons_result.py
@@ -1,7 +1,9 @@
-def person_result(details):
-    for i in len(details):
-     if result =="pass":
-     print(person_result.get(name))
-person_result([{'id': 1,‘result’:“pass”,'name': 'Ramesh’},{'id': 2,  ‘result’: “pass”, 'name': ‘Suresh'},{'id': 3,  ‘result’:“fail”, 'name': 'Alex'}]
-)
+def person_result(dictionary):
+    for i in dictionary:
+        if i['result'] == "pass":
+            print(i['name'])
 
+
+person_result([{'id': 1, 'result': "pass", 'name': 'Ramesh'},
+               {'id': 2, 'result': "pass", 'name': 'Suresh'},
+               {'id': 3, 'result': "fail", 'name': 'Alex'}])
