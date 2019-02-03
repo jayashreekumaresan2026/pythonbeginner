6512f2eb8b3b3e1a52f3ad6003ea937b631e15cc
Admin <admin@Intern6-MacBook-Pro.local>
2019-02-03 09:36:19 +0530

    day_assignment

--- Day3_assingment/wrap_words.py
+++ Day3_assingment/wrap_words.py
@@ -2,6 +2,6 @@ def wrap_words():
      my_array=[]
      list_array=input("enter the element to be present")
      for i in  range(1,len(list_array)+1):
-        my_array.append("{}".format(list_array[2:]+list_array[0:]))
+        my_array.append("{}".format(list_array[i:]+list_array[0:i]))
      return my_array
 print(wrap_words())
